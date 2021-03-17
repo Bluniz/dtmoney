7ab82217597e13b1376113d768eb68d9919a61c4
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-03-17 20:51:58 -0300

    Add colors of radio button when is clicked

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
   h2 {
@@ -58,13 +58,20 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: "red" | "green";
 }
+
+const colors = {
+  green: "#33CC95",
+  red: "#E52E4D",
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
-  background: ${({ isActive }) => (isActive ? "#cccc" : "transparent")};
+  background: ${({ isActive, activeColor }) =>
+    isActive ? transparentize(0.9, colors[activeColor]) : "transparent"};
 
   display: flex;
   align-items: center;
